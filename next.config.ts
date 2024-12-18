1fe1afe2d4873d7205a47cc0f2df42b359219281
Biango Christian <biangochristian@MacBook-Air-de-Biango.local>
2024-12-19 00:48:59 +0100

    fix(signup): #EEWA-41 rebase conflict error on nextConfig output

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  output: "export",
-
   output: "standalone",
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
